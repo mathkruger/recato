bc4f99e03f0879992e117b771fdfdd9313798b6e
Matheus Kruger <ms-kruger@hotmail.com>
2023-11-14 08:42:44 -0300

    melhorando o exemplo

--- example/js/components/header.js
+++ example/js/components/header.js
@@ -2,20 +2,37 @@ import Recato from "../../../recato.js";
 const { div, a, nav } = Recato;
 
 const Header = (router) => {
+  const routes = [
+    {
+      label: "Fausto",
+      url: "/fausto",
+    },
+    {
+      label: "Pokemon",
+      url: "/pokemon",
+    },
+  ];
+
   const component = nav(
     div(
       a("Sistemas de testes")
-        .attr("class", "navbar-item " + (router.currentUrl === '/' ? "is-active" : ""))
-        .attr("href", "#"),
+        .attr(
+          "class",
+          "navbar-item"
+        )
+        .attr("href", "#/")
     ).attr("class", "navbar-brand"),
     div(
       div(
-        a("Fausto")
-          .attr("class", "navbar-item " + (router.currentUrl === '/fausto' ? "is-active" : ""))
-          .attr("href", "#/fausto"),
-        a("Pokemon")
-          .attr("class", "navbar-item " + (router.currentUrl === '/pokemonn' ? "is-active" : ""))
-          .attr("href", "#/pokemon")
+        ...routes.map((x) =>
+          a(x.label)
+            .attr(
+              "class",
+              "navbar-item " +
+                (router.currentUrl === x.url ? "is-active" : "")
+            )
+            .attr("href", `#${x.url}`)
+        )
       ).attr("class", "navbar-start")
     ).attr("class", "navbar-menu is-active")
   ).attr("class", "navbar");
